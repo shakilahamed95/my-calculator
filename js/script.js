8f0df469c6b1abc6107636036d3b35e6f9077a10
Your Name <you@example.com>
2022-02-17 13:51:59 +0600

    some condition added

--- js/script.js
+++ js/script.js
@@ -6,54 +6,62 @@ function updateBalance(income, cost) {
     newBalance.innerText = remainingBalance;
 }
 function totalIncome() {
-    const totalIncome = parseFloat(document.getElementById("income-field").value);
-    return totalIncome;
+    const incomeInput = document.getElementById("income-field").value;
+    const totalIncome = parseFloat(incomeInput);
+    const error = document.getElementById('income-error')
+    if (totalIncome < 0 || isNaN(incomeInput)) {
+        error.style.display = 'block';
+    }
+    else {
+        error.style.display = 'none';
+        return totalIncome;
+    }
 }
 function totalCost() {
-    const foodCost = parseFloat(document.getElementById("food-cost").value);
-    const rentCost = parseFloat(document.getElementById("rent-cost").value);
-    const clothCost = parseFloat(document.getElementById("cloth-cost").value);
-    const totalCost = foodCost + rentCost + clothCost;
-    return totalCost;
+    const foodInput = document.getElementById("food-cost").value
+    const foodCost = parseFloat(foodInput);
+    const rentInput = document.getElementById("rent-cost").value;
+    const rentCost = parseFloat(rentInput);
+    const clothInput = document.getElementById("cloth-cost").value;
+    const clothCost = parseFloat(clothInput);
+    const inputError = document.getElementById('expenses-error');
+    const negativeError = document.getElementById('negative-error');
+    if (isNaN(foodInput) || isNaN(clothInput) || isNaN(rentInput)) {
+        inputError.style.display = 'block';
+        negativeError.style.display = 'none';
+    }
+    else if (foodCost < 0 || rentCost < 0 || clothCost < 0) {
+
+        negativeError.style.display = 'block';
+        inputError.style.display = 'none';
+    }
+    else {
+        negativeError.style.display = 'none';
+        inputError.style.display = 'none';
+        const totalCost = foodCost + rentCost + clothCost;
+        return totalCost;
+    }
 }
 document.getElementById("calculate-btn").addEventListener("click", function () {
     const income = totalIncome();
     const cost = totalCost();
     const incomeError = document.getElementById('calculation-error');
-    const inputError = document.getElementById('input-error');
-    const notNumberError = document.getElementById('notnumber-error');
     if (income < cost) {
         incomeError.style.display = 'block';
-        inputError.style.display = 'none';
-        notNumberError.style.display = 'none';
     }
-    else if (income < 0 || cost < 0) {
-        inputError.style.display = 'block';
-        incomeError.style.display = 'none';
-        notNumberError.style.display = 'none';
-    }
-    else if (isNaN(income) || isNaN(cost)) {
-        notNumberError.style.display = 'block';
-        inputError.style.display = 'none';
-        incomeError.style.display = 'none';
-    }
-
     else {
-        updateBalance(income, cost);
         incomeError.style.display = 'none';
-        inputError.style.display = 'none';
-        notNumberError.style.display = 'none';
-
+        updateBalance(income, cost);
     }
-
 })
 document.getElementById('save-btn').addEventListener('click', function () {
     const lowBalance = document.getElementById("low-balance")
+    const negativeInput = document.getElementById('not-number');
     const income = totalIncome()
     const cost = totalCost();
     const balance = income - cost;
-    const savingInput = document.getElementById('saving-input')
-    const savingRate = parseInt(savingInput.value);
+    const savingInput = document.getElementById('saving-input').value;
+    const savingRate = parseInt(savingInput);
     const savingMoney = (income * (savingRate / 100));
     const remainBalance = balance - savingMoney;
     if (remainBalance < 0) {
@@ -61,7 +69,14 @@ document.getElementById('save-btn').addEventListener('click', function () {
         document.getElementById("save-amount").innerText = "";
         document.getElementById("remaining").innerText = "";
     }
+    else if (isNaN(savingInput) || savingRate < 0) {
+        negativeInput.style.display = 'block'
+        lowBalance.style.display = 'none'
+        document.getElementById("save-amount").innerText = "";
+        document.getElementById("remaining").innerText = "";
+    }
     else {
+        negativeInput.style.display = 'none'
         document.getElementById("save-amount").innerText = savingMoney;
         document.getElementById("remaining").innerText = remainBalance;
         lowBalance.style.display = 'none'
